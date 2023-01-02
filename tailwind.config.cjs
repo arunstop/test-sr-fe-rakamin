ea9b1271dd4ffb43108c183db842c6a77dc34947
arunstop <arunstop@gmail.com>
2023-01-02 16:00:14 +0700

    add the required colors and font stylings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/ui/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/ui/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "nunito-sans": ["Nunito Sans", "ui-sans-serif", "system-u"],
+      inter: ["Inter", "ui-sans-serif", "system-u"],
+    },
+
+    extend: {
+      // colors: {
+      //   primary: "#01959F",
+      //   secondary: "#FEEABC",
+      //   danger: "#F5B1B7",
+      //   success: "#B8DBCA",
+      // },
+      backgroundColor: {
+        primary: "#F7FEFF",
+        secondary: "#FFFCF5",
+        danger: "#FFFAFA",
+        success: "#F8FBF9",
+      },
+      borderColor: {
+        primary: "#4DB5BC",
+        secondary: "#FEEABC",
+        danger: "#F5B1B7",
+        success: "#B8DBCA",
+      },
+      textColor: {
+        primary: "#01959F",
+        secondary: "#FA9810",
+        danger: "#E11428",
+        success: "#43936C",
+      },
+    },
   },
   plugins: [],
 }
