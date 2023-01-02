a7fe598d0291f93029d1e21a7d1f1cdf60cfa33a
arunstop <arunstop@gmail.com>
2023-01-02 22:50:18 +0700

    feat : impl card form on card add modal

--- src/ui/components/common/card/CardAddModal.tsx
+++ src/ui/components/common/card/CardAddModal.tsx
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import Modal, { IModalProps } from "../Modal"
 import Button from "../Button"
-import CardForm from "./CardForm"
+import CardForm, { ICardFormData } from "./CardForm"
 
 function CardAddModal({ ...modalProps }: IModalProps) {
+  const [data, setData] = useState({ name: "", progress: 0 })
+  const [onSubmit, setOnSubmit] = useState(() => {})
+  function onEdit(data: ICardFormData) {
+    setData(data)
+  }
   return (
-    <Modal {...modalProps}>
+    <Modal {...modalProps} >
       <div className="flex flex-col">
-        <CardForm onClose={modalProps.onClose}/>
+        <CardForm data={data} onEdit={onEdit} onClose={modalProps.onClose} />
       </div>
     </Modal>
   )
