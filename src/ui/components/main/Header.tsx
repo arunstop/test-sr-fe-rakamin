fae67f6d5fc455886020385567e6e21f2cbc558d
arunstop <arunstop@gmail.com>
2023-01-03 01:11:35 +0700

    style : fix header being shown behind task option buttons

--- src/ui/components/main/Header.tsx
+++ src/ui/components/main/Header.tsx
@@ -4,7 +4,7 @@ import { Icon } from "@iconify-icon/react"
 
 function Header({ title }: { title: string }) {
   return (
-    <nav className="sticky top-0 flex gap-2 sm:gap-4 border-b-2 border-[#E0E0E0] px-5 py-[1.125rem] bg-white">
+    <nav className="sticky top-0 flex gap-2 sm:gap-4 border-b-2 border-[#E0E0E0] px-5 py-[1.125rem] bg-white z-10">
       <h1 className="font-bold text-xl">{title || "App Name"}</h1>
       <Button className="text-sm">
         <Icon icon="uil:plus" />
