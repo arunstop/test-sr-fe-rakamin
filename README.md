4b5408623201f8173f44e8e0c3bd8764e5e33ca9
arunstop <arunstop@gmail.com>
2023-01-02 13:34:54 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# test-sr-fe-rakamin
---
+++ src/ui/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import LayoutMain from "../layouts/LayoutMain"
+
+function index() {
+  return <div>Initialized</div>
+}
+
+export default index
