c5d6b390aa4bb927b7fd662237c3d885f5f6687a
arunstop <arunstop@gmail.com>
2023-01-03 23:17:21 +0700

    fet : impl task addition

--- src/ui/components/common/card/CardAddModal.tsx
+++ src/ui/components/common/card/CardAddModal.tsx
@@ -1,20 +1,27 @@
-import React, { useState } from "react"
+import React, { useCallback, useState } from "react"
 import Modal, { IModalProps } from "../Modal"
 import Button from "../Button"
 import CardForm, { ICardFormData } from "./CardForm"
+import { useTaskStore } from "../../../../app/stores/task/TaskStore"
 
-function CardAddModal({ ...modalProps }: IModalProps) {
+function CardAddModal({ todoId,...modalProps }: IModalProps&{todoId:number}) {
   const [data, setData] = useState({ name: "", progress: 0 })
+  const { addTask } = useTaskStore()
   function onEdit(data: ICardFormData) {
     setData(data)
   }
-  function onSubmit(){
-    console.log(data)
-  }
+  const onSubmit = useCallback(() => {
+    addTask({ data: { input: data, todoId: todoId } })
+  }, [data])
   return (
-    <Modal {...modalProps} >
+    <Modal {...modalProps}>
       <div className="flex flex-col">
-        <CardForm data={data} onEdit={onEdit} onClose={modalProps.onClose}  onSubmit={onSubmit}/>
+        <CardForm
+          data={data}
+          onEdit={onEdit}
+          onClose={modalProps.onClose}
+          onSubmit={onSubmit}
+        />
       </div>
     </Modal>
   )
