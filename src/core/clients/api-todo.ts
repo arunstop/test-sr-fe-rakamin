6d64e91a58683b5cfebde6d7eb894bae62f0e683
arunstop <arunstop@gmail.com>
2023-01-03 15:21:32 +0700

    feat : added todo api client config

---
+++ src/core/clients/api-todo.ts
@@ -0,0 +1,2 @@
+export const API_BASE_URL = "https://todo-api-18-140-52-65.rakamin.com"
+export const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo4OSwiZXhwIjoxNjgxMzY3MjU2fQ.IJ6S-fwJEclKYo7ldhP3PKR6zm0B1MnW4N1sNmszPyk"
