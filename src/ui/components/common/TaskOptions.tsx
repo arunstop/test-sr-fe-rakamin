1ed41ccf1983465dac217a4171f1ee931883f56a
arunstop <arunstop@gmail.com>
2023-01-04 02:50:22 +0700

    style : hid task's movement action if thetodo is in either ends of the array

--- src/ui/components/common/TaskOptions.tsx
+++ src/ui/components/common/TaskOptions.tsx
@@ -5,6 +5,7 @@ import { Menu } from "@headlessui/react"
 export interface ICardOption {
   icon: ReactNode
   title: string
+  hidden?: boolean
   action: () => void
 }
 
@@ -22,20 +23,22 @@ function TaskOptions({
         className="absolute flex flex-col right-0 bg-white min-w-[12rem] rounded-lg overflow-hidden z-[1] py-2 
         shadow-[0px_4px_4px_rgba(0,0,0,0.08)]"
       >
-        {options.map((e,idx) => (
-          <Menu.Item key={idx}>
-            {({ active }) => (
-              <div
-                className={`flex gap-2 sm:gap-4 items-center px-4  py-1.5 font-bold text-[#333333]
+        {options.map((e, idx) =>
+          e.hidden ? null : (
+            <Menu.Item key={idx}>
+              {({ active }) => (
+                <div
+                  className={`flex gap-2 sm:gap-4 items-center px-4  py-1.5 font-bold text-[#333333]
                 ${!!active && "!text-primary"} cursor-pointer`}
-                onClick={e.action}
-              >
-                <span className="flex text-[24px]">{e.icon}</span>
-                <span>{e.title}</span>
-              </div>
-            )}
-          </Menu.Item>
-        ))}
+                  onClick={e.action}
+                >
+                  <span className="flex text-[24px]">{e.icon}</span>
+                  <span>{e.title}</span>
+                </div>
+              )}
+            </Menu.Item>
+          ),
+        )}
       </Menu.Items>
     </DropDown>
   )
