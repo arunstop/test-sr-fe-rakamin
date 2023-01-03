3781133ae47a10cf5ef4778d832b8ec7ed85abd6
arunstop <arunstop@gmail.com>
2023-01-04 03:13:12 +0700

    style : added placeholder comp

---
+++ src/ui/components/common/PlaceHolder.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+function PlaceHolder({ children }: { children: ReactNode }) {
+  return (
+    <span
+      className={`border rounded px-4 py-2 gap-3 bg-[#FAFAFA] border-[#e0e0e0] flex flex-col text-base text-[#757575]`}
+    >
+      {children}
+    </span>
+  )
+}
+
+export default PlaceHolder
