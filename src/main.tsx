1334b32c73f61f08d4d1a528b51b07fb989ff6e1
arunstop <arunstop@gmail.com>
2023-01-05 00:46:13 +0700

    fix : readded redirect util

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,13 @@ import TodoProvider from "./app/stores/todo/TodoProvider"
 import "./index.css"
 import App from "./ui/pages/index"
 
-// // middleware to redirect `/` to `/v1`
-// if (!(window.location.href + "/").includes(`${window.location.origin}/v1/`)) {
-//   window.history.replaceState("", "", "/v1" + window.location.pathname)
-// }
+// middleware to redirect `/` to `/v1`
+if (!(window.location.href + "/").includes(`${window.location.origin}/v1/`)) {
+  window.history.replaceState("", "", "/v1" + window.location.pathname)
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <BrowserRouter basename="v1">
       <Routes>
         <Route
@@ -24,4 +25,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
     </BrowserRouter>
+  </React.StrictMode>,
 )
