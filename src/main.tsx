67673a4e26d912fc7c358e43bcb912a79b6d0d4c
arunstop <arunstop@gmail.com>
2023-01-05 00:12:11 +0700

    feat : remove redirect util

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import TodoProvider from "./app/stores/todo/TodoProvider"
 import "./index.css"
 import App from "./ui/pages/index"
 
-// middleware to redirect `/` to `/v1`
-if (!(window.location.href + "/").includes(`${window.location.origin}/v1/`)) {
-  window.history.replaceState("", "", "/v1" + window.location.pathname)
-}
+// // middleware to redirect `/` to `/v1`
+// if (!(window.location.href + "/").includes(`${window.location.origin}/v1/`)) {
+//   window.history.replaceState("", "", "/v1" + window.location.pathname)
+// }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
