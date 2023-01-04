e1cee88f89fe3f3a425305ed2d8a1d82b9fcb24c
Arunstop <arunstop@gmail.com>
2023-01-04 23:59:33 +0700

    feat/impl subpath (#9)
    * style : add diffferent variants on card items
    
    * refactor : removing `direction` from move task params
    
    * style : reduct todo's description label
    
    * style : fix pixelated button's text when transitioning
    
    * style : fix font size config
    
    * style : fix default font-size usage
    
    * style : fix some padding/spacing mismatches
    
    * style : drag and drop wrapper now is card item itself
    
    * feat : impl custom base homepage
    
    * doc : added note for react router subpath problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./ui/pages/index"
-import "./index.css"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import TodoProvider from "./app/stores/todo/TodoProvider"
+import "./index.css"
+import App from "./ui/pages/index"
+
+// middleware to redirect `/` to `/v1`
+if (!(window.location.href + "/").includes(`${window.location.origin}/v1/`)) {
+  window.history.replaceState("", "", "/v1" + window.location.pathname)
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <TodoProvider>
-      <App />
-    </TodoProvider>
+    <BrowserRouter basename="v1">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <TodoProvider>
+              <App />
+            </TodoProvider>
+          }
+        />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
