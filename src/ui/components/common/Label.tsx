74f5535be11d848d34e66ba2034219584ead5a86
arunstop <arunstop@gmail.com>
2023-01-05 14:26:15 +0700

    test : added label

--- src/ui/components/common/Label.tsx
+++ src/ui/components/common/Label.tsx
@@ -2,10 +2,10 @@ import React from "react"
 import { TType } from "../../../app/types/commons"
 import { getTypeStyle } from "../../helpers/style"
 
-function Label({ text, type }: { text: string; type: TType }) {
-  const styleAll = getTypeStyle(type).all("bgMain")
+function Label({ text, type }: { text: string; type?: TType }) {
+  const styleAll = getTypeStyle(type || "primary").all("bgMain")
   return (
-    <span className={`border rounded px-2 text-xs py-0.5 ${styleAll}`}>
+    <span className={`border rounded px-2 text-xs py-0.5 ${styleAll}`} data-cy="label">
       {text}
     </span>
   )
