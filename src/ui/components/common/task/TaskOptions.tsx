c80f045d514a7058601110e8f627f51573a26578
arunstop <arunstop@gmail.com>
2023-01-05 18:27:47 +0700

    chore : fix dropdown application

--- src/ui/components/common/task/TaskOptions.tsx
+++ src/ui/components/common/task/TaskOptions.tsx
@@ -18,28 +18,22 @@ function TaskOptions({
 }) {
   return (
     <DropDown trigger={trigger}>
-      <Menu.Items
-        as="div"
-        className="absolute flex flex-col right-0 bg-white min-w-[12rem] rounded-lg overflow-hidden z-[1] py-2 
-        shadow-[0px_4px_4px_rgba(0,0,0,0.08)]"
-      >
-        {options.map((e, idx) =>
-          e.hidden ? null : (
-            <Menu.Item key={idx}>
-              {({ active }) => (
-                <div
-                  className={`flex gap-2 sm:gap-4 items-center px-4  py-1.5 font-bold text-[#333333]
+      {options.map((e, idx) =>
+        e.hidden ? null : (
+          <Menu.Item key={idx}>
+            {({ active }) => (
+              <div
+                className={`flex gap-2 sm:gap-4 items-center px-4  py-1.5 font-bold text-[#333333]
                 ${!!active && "!text-primary"} cursor-pointer`}
-                  onClick={e.action}
-                >
-                  <span className="flex text-[24px]">{e.icon}</span>
-                  <span>{e.title}</span>
-                </div>
-              )}
-            </Menu.Item>
-          ),
-        )}
-      </Menu.Items>
+                onClick={e.action}
+              >
+                <span className="flex text-[24px]">{e.icon}</span>
+                <span>{e.title}</span>
+              </div>
+            )}
+          </Menu.Item>
+        ),
+      )}
     </DropDown>
   )
 }
