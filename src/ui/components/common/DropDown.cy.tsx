ea5adb4f712ff4ac011c03bc818a23d3e6d7c6dd
arunstop <arunstop@gmail.com>
2023-01-05 19:18:16 +0700

    test : dropdown arrow keys test

--- src/ui/components/common/DropDown.cy.tsx
+++ src/ui/components/common/DropDown.cy.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import DropDown from "./DropDown"
 import Button from "./Button"
+import { Menu } from "@headlessui/react"
 
 describe("<DropDown />", () => {
   it("Renders", () => {
@@ -29,6 +30,7 @@ describe("<DropDown />", () => {
       </DropDown>,
     )
     cy.get(`[data-cy="dropdown"]`).as(`dropdown`).should("exist")
+    // click trigger
     cy.get(`@dropdown`).children().children().click()
     cy.get(`@dropdown`).children().last().should("exist")
   })
@@ -40,8 +42,9 @@ describe("<DropDown />", () => {
       </DropDown>,
     )
     cy.get(`[data-cy="dropdown"]`).as(`dropdown`).should("exist")
+    // click trigger
     cy.get(`@dropdown`).children().children().click()
-    cy.wait(500)
+    cy.wait(300)
     cy.get(`body`).click()
     // if children length == 1 it means the content is hidden
     cy.get(`@dropdown`).children().should("have.length", 1)
@@ -55,7 +58,7 @@ describe("<DropDown />", () => {
     )
     cy.get(`[data-cy="dropdown"]`).as(`dropdown`).should("exist")
     cy.get(`@dropdown`).children().first().click()
-    cy.wait(500)
+    cy.wait(300)
     cy.get(`@dropdown`).children().first().click()
     // if children length == 1 it means the content is hidden
     cy.get(`@dropdown`).children().should("have.length", 1)
@@ -68,10 +71,39 @@ describe("<DropDown />", () => {
       </DropDown>,
     )
     cy.get(`[data-cy="dropdown"]`).as(`dropdown`).should("exist")
+    // click trigger
     cy.get(`@dropdown`).children().children().click()
-    cy.wait(500)
+    cy.wait(300)
     cy.get(`body`).type(`{esc}`)
-    // cy.get(`@dropdown`).children().first().children().blur()
-    // cy.get(`@dropdown`).children().should("have.length", 1)
+  })
+
+  it("Selection with arrow keys", () => {
+    cy.mount(
+      <DropDown trigger={<Button children="dropdown" />}>
+        <Menu.Item>
+          {({active}) => <div data-cy="option-1">{"Option 1" + (active ? " <- ACTIVE":'')}</div>}
+        </Menu.Item>
+        <Menu.Item>
+          {({active}) => <div data-cy="option-2">{"Option 2" + (active ? " <- ACTIVE":'')}</div>}
+        </Menu.Item>
+        <Menu.Item>
+          {({active}) => <div data-cy="option-3">{"Option 3" + (active ? " <- ACTIVE":'')}</div>}
+        </Menu.Item>
+      </DropDown>,
+    )
+    cy.get(`[data-cy="dropdown"]`).as(`dropdown`).should("exist")
+    // click trigger
+    cy.get(`@dropdown`).children().children().click()
+    cy.wait(200)
+    // pressing arrows
+    cy.get(`body`).type(`{downArrow}`)
+    cy.get(`[data-cy="option-1"]`).should('contain.html', 'ACTIVE').should("exist")
+    cy.wait(200)
+    cy.get(`body`).type(`{downArrow}`)
+    cy.get(`[data-cy="option-2"]`).should('contain.html', 'ACTIVE').should("exist")
+    cy.wait(200)
+    cy.get(`body`).type(`{upArrow}`)
+    cy.get(`[data-cy="option-1"]`).should('contain.html', 'ACTIVE').should("exist")
+
   })
 })
