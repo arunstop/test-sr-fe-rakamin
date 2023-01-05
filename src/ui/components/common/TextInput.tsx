27461e4e4f39775d5897841767ab6b87604ef9fa
arunstop <arunstop@gmail.com>
2023-01-05 21:02:58 +0700

    chore : added ref forwarding to TextInput

--- src/ui/components/common/TextInput.tsx
+++ src/ui/components/common/TextInput.tsx
@@ -1,7 +1,16 @@
 import React from "react"
 
-function TextInput({...props}:React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} className={`bg-white text-[#404040] border-[#EDEDED] px-4 py-2 rounded-lg border-2 ${props.className}`} />
-}
+const TextInput = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      {...props}
+      className={`bg-white text-[#404040] border-[#EDEDED] px-4 py-2 rounded-lg border-2 ${props.className}`}
+    />
+  )
+})
 
 export default TextInput
