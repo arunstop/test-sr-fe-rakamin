c019d6a9c1b3d63b4234fdf203ccd4424d66ec62
arunstop <arunstop@gmail.com>
2023-01-06 02:04:27 +0700

    chore : set `testIsolation` to false

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+import { API_BASE_URL } from './src/core/clients/api-todo';
 import { defineConfig } from "cypress";
 
 export default defineConfig({
@@ -5,6 +6,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    testIsolation:false,
+    baseUrl:API_BASE_URL
   },
 
   component: {
