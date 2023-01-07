2d692c23f21c51825048dad9dbf4b148769f3cc4
arunstop <arunstop@gmail.com>
2023-01-07 23:31:40 +0700

    feat : added drag to scroll

--- src/ui/pages/index.tsx
+++ src/ui/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useRef, useState } from "react"
 import LayoutMain from "../layouts/LayoutMain"
 import TodoItem, { ICardDirection } from "../components/common/todo/TodoItem"
 import { TType } from "../../app/types/commons"
@@ -29,24 +29,100 @@ function PageIndex() {
     else if (n % 1 == 0) return "primary"
     return "primary"
   }, [])
+
+  const ref = useRef<HTMLElement | null>(null)
+  const dragScroll = useDragScroll(ref)
   return (
     <LayoutMain>
-        <main className="p-6 flex [&>*]:flex-none flex-1 [&>*]:w-[326px]  gap-4 max-w-screen overflow-auto" data-cy="wrapper-todos">
-          <>
-            {state?.data.map((v, idx) => {
-              return (
-                <TodoItem
-                  key={idx}
-                  todo={v}
-                  type={getType(idx+1)}
-                  direction={getDirection(v)}
-                />
-              )
-            })}
-          </>
-        </main>
+      <main
+        className="p-6 flex [&>*]:flex-none flex-1 [&>*]:w-[326px]  gap-4 max-w-screen overflow-auto items-start"
+        data-cy="wrapper-todos"
+        ref={(e) => {
+          ref.current = e
+        }}
+        {...dragScroll}
+      >
+        <>
+          {state?.data.map((v, idx) => {
+            return (
+              <TodoItem
+                key={idx}
+                todo={v}
+                type={getType(idx + 1)}
+                direction={getDirection(v)}
+              />
+            )
+          })}
+        </>
+      </main>
     </LayoutMain>
   )
+}
+
+const useDragScroll = <EL extends HTMLElement>(
+  ref: React.RefObject<EL | null>,
+) => {
+  const el = ref.current
+  const [pos, setPos] = useState<{
+    // scrolling distance of the element
+    elLeft: number
+    elTop: number
+    // mouse location
+    mouseX: number
+    mouseY: number
+    isScrolling: boolean
+  }>({
+    elLeft: 0,
+    elTop: 0,
+    mouseX: 0,
+    mouseY: 0,
+    isScrolling: false,
+  })
+
+  function setCursorStyle(scrolling: boolean) {
+    if (!el) return
+    if (scrolling) {
+      el.style.cursor = "grabbing"
+      el.style.userSelect = "none"
+      return
+    }
+    el.style.cursor = "default"
+    el.style.userSelect = "auto"
+  }
+  
+  function onMouseDown(ev: React.MouseEvent) {
+    if (!el) return
+    setPos({
+      // scrolling distance of the element
+      elLeft: el.scrollLeft,
+      elTop: el.scrollTop,
+      // mouse location
+      mouseX: ev.clientX,
+      mouseY: ev.clientY,
+      isScrolling: true,
+    })
+    setCursorStyle(true)
+  }
+
+  function onMouseUp(ev: React.MouseEvent) {
+    if (!el) return
+    setPos({ ...pos, isScrolling: false })
+    setCursorStyle(false)
+  }
+
+  function onMouseMove(ev: React.MouseEvent) {
+    if (!el || !pos.isScrolling) return
+    const dx = ev.clientX - pos.mouseX
+    const dy = ev.clientY - pos.mouseY
+    el.scrollLeft = pos.elLeft - dx
+    el.scrollTop = pos.elTop - dy
+  }
+
+  return {
+    onMouseDown,
+    onMouseUp,
+    onMouseMove,
+  }
 }
 
 export default PageIndex
