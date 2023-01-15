bfade830c2f73541aa3329b20a451c703baecbc7
arunstop <arunstop@gmail.com>
2023-01-15 22:12:21 +0700

    feat : redirect from `/auth` to `/` when it's logged in

--- src/ui/pages/auth.tsx
+++ src/ui/pages/auth.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 import LayoutAuth from "../layouts/LayoutAuth"
 import AuthPanel from "../components/auth/AuthPanel"
+import { useAuthStore } from "../../app/stores/auth/AuthStore."
+import { Navigate } from "react-router-dom"
 
 function auth() {
+  const { email } = useAuthStore()
   return (
-    <LayoutAuth>
-      <AuthPanel />
-    </LayoutAuth>
+    <>
+      {!!email && <Navigate to="/" replace={true} />}
+      <LayoutAuth>
+        <AuthPanel />
+      </LayoutAuth>
+    </>
   )
 }
 
